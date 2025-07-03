1e061f8acfa73c40cf7e1e2c2e8eff2f702fd5e8
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-03 19:16:43 +0700

    Addition TestNG Annotation help with Inheritance to remove boilerplate code in Test

--- src/test/java/PS.java
+++ src/test/java/PS.java
@@ -1,5 +1,20 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class PS {
+
     public void doThis(){
         System.out.println("I am here");
     }
+
+    @BeforeMethod
+    public void BeforeRun(){
+        System.out.println("Run me first");
+    }
+
+    @AfterMethod
+    public void AfterRun(){
+        System.out.println("Run me last");
+    }
+
 }
--- src/test/java/PS1.java
+++ src/test/java/PS1.java
@@ -1,6 +1,8 @@
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PS1 extends PS{
+
     @Test
     public void testRun(){
         doThis();
