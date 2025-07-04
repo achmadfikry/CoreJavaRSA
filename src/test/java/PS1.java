d8d66aa0448e1dee3b2299844f6b562fa0423fd2
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-04 10:33:52 +0700

    Addition parameterized constructor, instance variable & this keyword

--- src/test/java/PS1.java
+++ src/test/java/PS1.java
@@ -5,6 +5,10 @@ public class PS1 extends PS{
 
     @Test
     public void testRun(){
-        doThis();
+        int a = 3;
+        PS2 ps2 = new PS2(a); //create object of the class, //constructor
+        System.out.println(ps2.increment());
+        System.out.println(ps2.decrement());
+        doThis(); //inheritance
     }
 }
--- src/test/java/PS2.java
+++ src/test/java/PS2.java
@@ -1,2 +1,17 @@
 public class PS2 {
+    int a; //this variable a is global variable
+
+    public PS2(int a) { //this parameter a is an intance variable
+        this.a = a; //change the global variable equals to instance variable
+    }
+
+    public int increment(){
+        a = a + 1;
+        return a;
+    }
+
+    public int decrement(){
+        a = a - 1;
+        return a;
+    }
 }
